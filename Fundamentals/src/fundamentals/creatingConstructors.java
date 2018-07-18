314f2b79ae53941e7cebdcf9992dad19224548fd
josuerosa <josuerosa@192.168.0.102>
2018-07-17 23:22:30 -0400

    Added new packages

---
+++ Fundamentals/src/fundamentals/creatingConstructors.java
@@ -0,0 +1,35 @@
+package fundamentals;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author josuerosa
+ */
+public class creatingConstructors {
+    int age;
+    
+    
+    public creatingConstructors(int ageYears)
+            
+    {
+    
+    age = ageYears;
+    //A constructor allow you to define the behavior of a class.
+    
+    
+    }
+    
+    
+    public static void main (String[] args)
+    {
+        
+    }
+    
+    
+}
